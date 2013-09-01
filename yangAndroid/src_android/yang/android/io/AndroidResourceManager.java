66ab03372410fe0fbeff215f04dcd3bd3ff8b5da
Dieter Eberle <d.eberle@gmx.com>
2013-09-01 15:28:03 +0200

    external storage support for android

--- yangAndroid/src_android/yang/android/io/AndroidResourceManager.java
+++ yangAndroid/src_android/yang/android/io/AndroidResourceManager.java
@@ -1,24 +1,26 @@
 package yang.android.io;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
 
-import yang.model.DebugYang;
 import yang.systemdependent.AbstractResourceManager;
 import android.content.Context;
+import android.os.Environment;
 
 public class AndroidResourceManager extends AbstractResourceManager {
 
 	private Context mContext;
-	
+
 	public AndroidResourceManager(Context context) {
 		mContext = context;
 	}
-	
+
 	@Override
 	public InputStream getInputStream(String filename){
 		InputStream is = null;
@@ -29,7 +31,7 @@ public class AndroidResourceManager extends AbstractResourceManager {
 		}
 		return is;
 	}
-	
+
 	@Override
 	public Properties loadPropertiesFile(String filename) {
 		Properties props = new Properties();
@@ -44,35 +46,57 @@ public class AndroidResourceManager extends AbstractResourceManager {
 
 	@Override
 	public String[] getFileList(String directory) throws IOException {
-		if(directory.endsWith(File.separator)) 
+		if(directory.endsWith(File.separator))
 			directory = directory.substring(0,directory.length()-1);
 		return mContext.getAssets().list(directory);
 	}
-	
+
 	@Override
 	public InputStream getFileSystemInputStream(String filename) throws FileNotFoundException {
-		return mContext.openFileInput(filename);
+		return new FileInputStream(new File(mContext.getFilesDir(), filename));
 	}
-	
+
 	@Override
 	public OutputStream getFileSystemOutputStream(String filename) throws FileNotFoundException {
-		return mContext.openFileOutput(filename, Context.MODE_PRIVATE);
+		File newFile = new File(mContext.getFilesDir(), filename);
+		File parent = newFile.getParentFile();
+		if (!parent.exists()) parent.mkdirs();
+		return new FileOutputStream(newFile);
 	}
-	
+
 	@Override
 	public boolean fileExistsInFileSystem(String filename) {
-		//return new File(mContext.getFilesDir()+""+File.pathSeparatorChar+filename).exists();
-		try{
-			getFileSystemInputStream(filename);
-			return true;
-		}catch(Exception ex) {
-			return false;
-		}
+		return new File(mContext.getFilesDir(), filename).exists();
 	}
-	
-//	@Override
-//	public OutputStream getFileSystemOutputStream(String filename) throws FileNotFoundException {
-//		
-//	}
 
+	@Override
+	public OutputStream getExternalFileSystemOutputStream(String filename) throws FileNotFoundException {
+		File file = new File(getExternalStorageDir(), filename);
+		File parent = file.getParentFile();
+		if (!parent.exists()) {
+			parent.mkdirs();
+		}
+		return new FileOutputStream(file);
+	}
+
+	@Override
+	public InputStream getExteralFileSystemInputStream(String filename) throws FileNotFoundException {
+		File file = new File(getExternalStorageDir(), filename);
+		return new FileInputStream(file);
+	}
+
+	@Override
+	public boolean fileExistsInExternalFileSystem(String filename) {
+		return new File(getExternalStorageDir(), filename).exists();
+	}
+
+	private File getExternalStorageDir() {
+		String state = Environment.getExternalStorageState();
+		if (Environment.MEDIA_MOUNTED.equals(state)) {
+			File externalDir = new File(Environment.getExternalStorageDirectory(), mContext.getPackageName());
+			if (!externalDir.exists()) externalDir.mkdir();
+			return externalDir;
+		}
+		throw new RuntimeException("external storage not available");
+	}
 }
