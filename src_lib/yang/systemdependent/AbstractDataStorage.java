d3845556f93fe5f90b25cccac829c5f98ccadb9c
Dieter Eberle <d.eberle@gmx.com>
2013-09-30 13:14:48 +0200

    add putString to DataStorage

--- src_lib/yang/systemdependent/AbstractDataStorage.java
+++ src_lib/yang/systemdependent/AbstractDataStorage.java
@@ -1,14 +1,16 @@
 package yang.systemdependent;
 
 public abstract class AbstractDataStorage {
-	
+
 	protected static final String FILENAME = "storage";
-	
+
 	public abstract void putFloat(String key, float value);
 	public abstract void putInt(String key, int value);
 	public abstract void putBoolean(String key, boolean value);
-	
+	public abstract void putString(String key, String value);
+
 	public abstract float getFloat(String key, float defaultValue);
 	public abstract int getInt(String key, int defaultValue);
 	public abstract boolean getBoolean(String key, boolean defaultValue);
+	public abstract String getString(String key, String defaultValue);
 }
