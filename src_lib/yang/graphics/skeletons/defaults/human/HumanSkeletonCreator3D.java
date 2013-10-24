e52cc25558c28148536cb073440c824973295b15
Progga <fenderandreas@web.de>
2013-10-24 22:32:06 +0200

    human 3D skeleton additional breast and hips bone

--- src_lib/yang/graphics/skeletons/defaults/human/HumanSkeletonCreator3D.java
+++ src_lib/yang/graphics/skeletons/defaults/human/HumanSkeletonCreator3D.java
@@ -1,6 +1,5 @@
 package yang.graphics.skeletons.defaults.human;
 
-import yang.graphics.skeletons.CartoonBone;
 import yang.physics.massaggregation.MassAggregation;
 import yang.physics.massaggregation.elements.Bone3D;
 import yang.physics.massaggregation.elements.Joint;
@@ -20,12 +19,12 @@ public class HumanSkeletonCreator3D {
 	public Joint mRightHandJoint;
 	public Joint mLeftToesJoint;
 	public Joint mRightToesJoint;
-	
+
 	public Joint mLeftShoulderJoint;
 	public Joint mRightShoulderJoint;
 	public Joint mLeftLegJoint;
 	public Joint mRightLegJoint;
-	
+
 	public Bone3D mBodyBone;
 	public Bone3D mHeadBone;
 	public Bone3D mLeftUpperArmBone;
@@ -36,39 +35,42 @@ public class HumanSkeletonCreator3D {
 	public Bone3D mRightUpperLegBone;
 	public Bone3D mLeftLowerLegBone;
 	public Bone3D mRightLowerLegBone;
-	
+
 	public Bone3D mLeftShoulderBone;
 	public Bone3D mRightShoulderBone;
 	public Bone3D mLeftLegBone;
 	public Bone3D mRightLegBone;
-	
+
+	public Bone3D mHipsBone;
+	public Bone3D mBreastBone;
+
 	public MassAggregation mSkeleton;
-	
+
 	public HumanSkeletonCreator3D() {
-		
+
 	}
-	
+
 	public MassAggregation create(HumanSkeletonProperties properties) {
-		
+
 		if(mSkeleton==null) {
 			mSkeleton = new MassAggregation();
 		}
-			
-		float locScaleX = 1;
-		float locScaleY = 1;
-		float smallRad = 0.05f*locScaleX;
-		float neckY = (1-properties.mHeadRatio)*properties.mHeight;
-		float hipsY = properties.mHeight*properties.mHipRatio;
-		float kneeY = hipsY*0.5f;
-		float legsX = properties.mHipWidth*0.5f;
-		float shoulderX = properties.mShoulderWidth*0.5f;
-		float shoulderY = neckY-properties.mShoulderOffsetY*properties.mHeight;
-		float armX = properties.mShoulderWidth*0.5f;
-		float elbowY = shoulderY-(properties.mArmLength*0.5f)*properties.mHeight;
-		float handY = shoulderY-properties.mArmLength*properties.mHeight;
-	
+
+		final float locScaleX = 1;
+		final float locScaleY = 1;
+		final float smallRad = 0.05f*locScaleX;
+		final float neckY = (1-properties.mHeadRatio)*properties.mHeight;
+		final float hipsY = properties.mHeight*properties.mHipRatio;
+		final float kneeY = hipsY*0.5f;
+		final float legsX = properties.mHipWidth*0.5f;
+		final float shoulderX = properties.mShoulderWidth*0.5f;
+		final float shoulderY = neckY-properties.mShoulderOffsetY*properties.mHeight;
+		final float armX = properties.mShoulderWidth*0.5f;
+		final float elbowY = shoulderY-(properties.mArmLength*0.5f)*properties.mHeight;
+		final float handY = shoulderY-properties.mArmLength*properties.mHeight;
+
 		mSkeleton.mDefaultJointRadius = 0.1f*locScaleX;
-		
+
 		mBreastJoint = mSkeleton.addJoint("Breast",null, 0,neckY,0);
 		mHeadJoint = mSkeleton.addJoint("Head",mBreastJoint, 0,2*locScaleY,0).setRadius(0.15f*locScaleX);
 		mHipJoint = mSkeleton.addJoint("Hip",mBreastJoint, 0,hipsY,0);
@@ -84,7 +86,7 @@ public class HumanSkeletonCreator3D {
 		mRightElbowJoint = mSkeleton.addJoint("RightElbow",mRightShoulderJoint, -armX,elbowY,0);
 		mLeftHandJoint = mSkeleton.addJoint("LeftHand",mLeftElbowJoint, armX, handY, 0);
 		mRightHandJoint = mSkeleton.addJoint("RightHand",mRightElbowJoint, -armX, handY, 0);
-		
+
 		mSkeleton.mDefaultBoneSpring = 10;
 		mBodyBone = mSkeleton.addSpringBone(new Bone3D("Body",mBreastJoint,mHipJoint));
 		mBodyBone = mSkeleton.addSpringBone(new Bone3D("Head",mBreastJoint,mHeadJoint));
@@ -95,14 +97,16 @@ public class HumanSkeletonCreator3D {
 		mLeftLowerArmBone = mSkeleton.addSpringBone(new Bone3D("LeftLowerArm",mLeftElbowJoint,mLeftHandJoint));
 		mRightLowerArmBone = mSkeleton.addSpringBone(new Bone3D("RightLowerArm",mRightElbowJoint,mRightHandJoint));
 		mLeftLegBone = mSkeleton.addSpringBone(new Bone3D("LeftLeg",mHipJoint,mLeftLegJoint));
-		mRightLegBone = mSkeleton.addSpringBone(new Bone3D("RightLeg",mHipJoint,mRightLegJoint));	
+		mRightLegBone = mSkeleton.addSpringBone(new Bone3D("RightLeg",mHipJoint,mRightLegJoint));
 		mLeftUpperLegBone = mSkeleton.addSpringBone(new Bone3D("LeftUpperLeg",mLeftLegJoint,mLeftKneeJoint));
 		mRightUpperLegBone = mSkeleton.addSpringBone(new Bone3D("RightUpperLeg",mRightLegJoint,mRightKneeJoint));
 		mLeftLowerLegBone = mSkeleton.addSpringBone(new Bone3D("LeftLowerLeg",mLeftKneeJoint,mLeftFootJoint));
 		mRightLowerLegBone = mSkeleton.addSpringBone(new Bone3D("RightLowerLeg",mRightKneeJoint,mRightFootJoint));
-		
-		
+
+		mBreastBone = mSkeleton.addSpringBone(new Bone3D("Breast",mLeftShoulderJoint,mRightShoulderJoint));
+		mHipsBone = mSkeleton.addSpringBone(new Bone3D("Hips",mLeftLegJoint,mRightLegJoint));
+
 		return mSkeleton;
 	}
-	
+
 }
