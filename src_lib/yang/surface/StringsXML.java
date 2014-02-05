04d8e77a58d0a2bc3949246e1cb9f9f44d305fac
Dieter Eberle <d.eberle@gmx.com>
2014-02-05 23:13:18 +0100

    use SAX instead of DOM for parsing the strings.xml

--- src_lib/yang/surface/StringsXML.java
+++ src_lib/yang/surface/StringsXML.java
@@ -5,25 +5,24 @@ import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 
 import yang.graphics.font.DrawableAnchoredLines;
 
-//TODO xml comments not working
 public class StringsXML {
 
 	public static final String UNKNOWN_KEY = "NONE";
-	
+
 	private static final String XML_HEADER = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
 	private static final String RESOURCES_OPEN = "<resources>";
 	private static final String RESOURCES_CLOSE = "</resources>";
-	
+
 	private final HashMap<String, String> mStrings;
 	private StringsXML mFallbackStringsXML = null;
 
@@ -48,7 +47,7 @@ public class StringsXML {
 
 	public StringsXML(boolean createFallback) {
 		mStrings = new HashMap<String, String>();
-		
+
 		if(createFallback) {
 			mFallbackStringsXML = new StringsXML();
 		}
@@ -57,34 +56,21 @@ public class StringsXML {
 	public StringsXML load(InputStream xmlStream) {
 		return load(xmlStream, this);
 	}
-	
+
 	public StringsXML loadFallback(InputStream xmlStream) {
 		return load(xmlStream, mFallbackStringsXML);
 	}
-	
+
 	private final static StringsXML load(InputStream xmlStream, StringsXML stringsXML) {
 		stringsXML.mStrings.clear();
 		try {
-			final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			final DocumentBuilder db = dbf.newDocumentBuilder();
-			final Document doc = db.parse(xmlStream);
-
-			final Element root = doc.getDocumentElement();
-			final NodeList strings = root.getElementsByTagName("string");
-			final int amount = strings.getLength();
-
-			for (int i = 0; i < amount; ++i) {
-				final Node node = strings.item(i);
-				final String string = node.getTextContent().trim().replace("\n", "\0").replace("\t", "\0").replace("\\n", "\n").replace("\\t", "\t").replace("\\'", "'");
-				final String key = node.getAttributes().getNamedItem("name").getTextContent();
-				stringsXML.mStrings.put(key, string);
-			}
-
+			XMLReader reader = XMLReaderFactory.createXMLReader();
+			reader.setContentHandler(new StringXMLParser(stringsXML));
+			reader.parse(new InputSource(xmlStream));
 			xmlStream.close();
-		} catch (final Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 		return stringsXML;
 	}
 
@@ -129,11 +115,58 @@ public class StringsXML {
 	public void save(PrintStream stream) {
 		stream.println(XML_HEADER);
 		stream.println(RESOURCES_OPEN);
-		
+
 		for(Entry<String,String> e: mFallbackStringsXML.mStrings.entrySet()){
 			stream.println("\t<string name=\""+e.getKey()+ "\">"+e.getValue()+"</string>");
 		}
-		stream.print(RESOURCES_CLOSE);		
+		stream.print(RESOURCES_CLOSE);
 	}
 
+
+	private static class StringXMLParser implements ContentHandler {
+
+		private String mCurString;
+		private String mCurKey;
+		private StringsXML mStrings;
+
+		public StringXMLParser(StringsXML strings) {
+			mStrings = strings;
+		}
+
+		@Override
+		public void characters(char[] ch, int start, int length) throws SAXException {
+			mCurString = new String(ch, start, length);
+			mCurString.trim().replace("\n", "\0").replace("\t", "\0").replace("\\n", "\n").replace("\\t", "\t").replace("\\'", "'");
+		}
+
+		@Override
+		public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+			if (localName.equals("string")) {
+				mCurKey = atts.getValue("name");
+			}
+		}
+
+		@Override
+		public void endElement(String uri, String localName, String qName) throws SAXException {
+			if (localName.equals("string")) {
+				mStrings.mStrings.put(mCurKey, mCurString);
+			}
+		}
+
+		@Override public void endDocument() throws SAXException {}
+
+		@Override public void startPrefixMapping(String suffix, String uri) throws SAXException {}
+
+		@Override public void endPrefixMapping(String prefix) throws SAXException {}
+
+		@Override public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {}
+
+		@Override public void processingInstruction(String target, String data) throws SAXException {}
+
+		@Override public void setDocumentLocator(Locator locator) {}
+
+		@Override public void skippedEntity(String name) throws SAXException {}
+
+		@Override public void startDocument() throws SAXException {}
+	}
 }
