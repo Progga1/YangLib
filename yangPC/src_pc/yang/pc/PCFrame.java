220af0af34892a6463fe11c6a96062616d9144c6
Didi <d.eberle@gmx.com>
2014-03-06 10:26:44 +0100

    fix alt-key focus issue in jframe

--- yangPC/src_pc/yang/pc/PCFrame.java
+++ yangPC/src_pc/yang/pc/PCFrame.java
@@ -1,7 +1,10 @@
 package yang.pc;
 
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
@@ -74,13 +77,24 @@ public class PCFrame extends JFrame implements WindowListener,FocusListener  {
 
 	}
 
+	private final KeyEventDispatcher altDisabler = new KeyEventDispatcher() {
+        @Override
+        public boolean dispatchKeyEvent(KeyEvent e) {
+            return e.getKeyCode() == 18;
+        }
+    };
+
 	@Override
 	public void focusGained(FocusEvent arg0) {
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(altDisabler);
 		//mSurface.resume();
+
 	}
 
 	@Override
 	public void focusLost(FocusEvent arg0) {
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().removeKeyEventDispatcher(altDisabler);
+
 //		if(mFirstFocLost) {
 //			mFirstFocLost = false;
 //			return;
