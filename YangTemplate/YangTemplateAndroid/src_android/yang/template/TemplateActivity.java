414b9554373388d064d65f9628b9f3a3244e939e
Didi <d.eberle@gmx.com>
2014-05-14 13:26:02 +0200

    fix template project, set compile target to newest android version (api 19)

--- YangTemplate/YangTemplateAndroid/src_android/yang/template/TemplateActivity.java
+++ YangTemplate/YangTemplateAndroid/src_android/yang/template/TemplateActivity.java
@@ -1,10 +1,11 @@
 package yang.template;
 
 import samplesurface.SampleSurface;
+import yang.android.graphics.YangActivity;
 import android.os.Bundle;
 
-public class TemplateActivity  extends YangActivity {
-	
+public class TemplateActivity extends YangActivity {
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
