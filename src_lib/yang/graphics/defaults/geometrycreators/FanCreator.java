e2e7069d3cf1cc6073bead5a7cfbbdbff345041e
Xider1337 <der.conni@gmail.com>
2014-05-21 23:47:23 +0200

    FanCreator based on relative indices

---
+++ src_lib/yang/graphics/defaults/geometrycreators/FanCreator.java
@@ -0,0 +1,100 @@
+package yang.graphics.defaults.geometrycreators;
+
+import yang.graphics.buffers.IndexedVertexBuffer;
+import yang.graphics.defaults.DefaultGraphics;
+import yang.graphics.translator.GraphicsTranslator;
+
+public class FanCreator {
+	
+	public DefaultGraphics<?> mGraphics;
+	public GraphicsTranslator mTranslator;
+	public IndexedVertexBuffer mVertexBuffer;
+	
+	protected int id = 0;
+	private int d;
+	
+	/**
+	 * initializes a FanCreator to a certain degree (1-lowest)
+	 * @param graphics
+	 * @param degree 1: default triangle fan (at least 3 Points needed) <br>
+	 * 2: dual row triangle fan (at least 5 Points needed) <br>
+	 * n: n-row triangle fan (at least 2*n+1 Points needed)
+	 */
+	public FanCreator(DefaultGraphics<?> graphics, int degree) {
+		mGraphics = graphics;
+		mTranslator = graphics.mTranslator;
+		
+		d = degree;
+	}
+	
+
+	/**
+	 * Sets the center of the fan
+	 * @param x
+	 * @param y
+	 */
+	public void startFan(float x, float y) {
+		id = 0;
+		mVertexBuffer = mGraphics.mCurrentVertexBuffer;
+		mGraphics.putPosition(x, y);		
+	}
+	
+	/**
+	 * Adds one point to the fan. 
+	 * For a fan of degree n, n points are needed to complete one column of the fan.
+	 * Continue has to be called n*columns to have a complete fan 
+	 * @param x
+	 * @param y
+	 */
+	public void continueFan(float x, float y) {
+		id++;
+		
+		if( id <= d ) { 
+			mGraphics.putPosition(x,y);			
+			
+		} else {			
+			
+			if(id % d == 1 || d==1) {
+				mVertexBuffer.putRelativeIndex(0);
+				mVertexBuffer.putRelativeIndex(-d);
+				mVertexBuffer.putRelativeIndex(-id);			
+				
+			} else {
+				
+				mVertexBuffer.putRelativeIndex(0);
+				mVertexBuffer.putRelativeIndex(-1);
+				mVertexBuffer.putRelativeIndex(-d);
+				
+				mVertexBuffer.putRelativeIndex(-1);
+				mVertexBuffer.putRelativeIndex(-d-1);
+				mVertexBuffer.putRelativeIndex(-d);
+			}
+			
+			mGraphics.putPosition(x,y);			
+		}		
+	}
+
+
+	/**
+	 * connects the first with the last column
+	 */
+	public void closeFan() {
+		
+		mVertexBuffer.putRelativeIndex(-id-1);
+		mVertexBuffer.putRelativeIndex(-id);
+		mVertexBuffer.putRelativeIndex(-d);
+		
+		for(int i = 1; i<d; i++) {			
+			mVertexBuffer.putRelativeIndex( -i-1);
+			mVertexBuffer.putRelativeIndex( -i);
+			mVertexBuffer.putRelativeIndex(-id+d-i);
+			
+			mVertexBuffer.putRelativeIndex( -i-1);
+			mVertexBuffer.putRelativeIndex( -id+d-i-1);
+			mVertexBuffer.putRelativeIndex(-id+d-i);	
+			
+		}
+	}
+	
+	
+}
