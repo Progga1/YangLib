76be7ce14620cf4d4a28e34ef9c58f69424be059
Didi <d.eberle@gmx.com>
2014-08-22 18:03:04 +0200

    remove obsolete method

--- src_lib/yang/math/objects/Quadruple.java
+++ src_lib/yang/math/objects/Quadruple.java
@@ -9,23 +9,23 @@ public class Quadruple {
 	public static final Quadruple ONE = new Quadruple(1,1,1,1);
 	public static final Quadruple Q0011 = new Quadruple(0,0,1,1);
 	public static final Quadruple Q0101 = new Quadruple(0,1,0,1);
-	
+
 	public float[] mValues;
-	
+
 	public Quadruple() {
 		mValues = new float[4];
 	}
-	
+
 	public Quadruple(float v1,float v2,float v3,float v4) {
 		this();
 		set(v1,v2,v3,v4);
 	}
-	
+
 	public Quadruple(float[] values) {
 		this();
 		set(values);
 	}
-	
+
 	@Override
 	public String toString()
 	{
@@ -38,62 +38,62 @@ public class Quadruple {
 		mValues[2] = v3;
 		mValues[3] = v4;
 	}
-	
+
 	public void set(float v1, float v2, float v3) {
 		mValues[0] = v1;
 		mValues[1] = v2;
 		mValues[2] = v3;
 	}
-	
+
 	public void set(Quadruple preface) {
 		mValues[0] = preface.mValues[0];
 		mValues[1] = preface.mValues[1];
 		mValues[2] = preface.mValues[2];
 		mValues[3] = preface.mValues[3];
 	}
-	
+
 	public void set(float[] prefaceValues) {
 		mValues[0] = prefaceValues[0];
 		mValues[1] = prefaceValues[1];
 		mValues[2] = prefaceValues[2];
 		mValues[3] = prefaceValues[3];
 	}
-	
+
 	public void add(float v1,float v2,float v3,float v4) {
 		mValues[0] += v1;
 		mValues[1] += v2;
 		mValues[2] += v3;
 		mValues[3] += v4;
 	}
-	
+
 	public void add(Quadruple values) {
 		mValues[0] += values.mValues[0];
 		mValues[1] += values.mValues[1];
 		mValues[2] += values.mValues[2];
 		mValues[3] += values.mValues[3];
 	}
-	
+
 	public void subtract(float v1,float v2,float v3,float v4) {
 		mValues[0] -= v1;
 		mValues[1] -= v2;
 		mValues[2] -= v3;
 		mValues[3] -= v4;
 	}
-	
+
 	public void subtract(Quadruple values) {
 		mValues[0] -= values.mValues[0];
 		mValues[1] -= values.mValues[1];
 		mValues[2] -= values.mValues[2];
 		mValues[3] -= values.mValues[3];
 	}
-	
+
 	public void writeIntoBuffer(ByteBuffer source) {
 		source.put((byte)(mValues[0]*255));
 		source.put((byte)(mValues[1]*255));
 		source.put((byte)(mValues[2]*255));
 		source.put((byte)(mValues[3]*255));
 	}
-	
+
 	/**
 	 * weight=0 => result=quad1
 	 * weight=1 => result=quad2
@@ -105,18 +105,9 @@ public class Quadruple {
 		mValues[2] = quad1.mValues[2]*dWeight+quad2.mValues[2]*weight;
 		mValues[3] = quad1.mValues[3]*dWeight+quad2.mValues[3]*weight;
 	}
-	
+
 	@Override
 	public Quadruple clone() {
 		return new Quadruple(mValues);
 	}
-
-	public void mixIn(float amountNew, float... color) {
-		final float old = 1-amountNew;		
-		
-		for(int i=0; i<mValues.length; i++) {
-			mValues[i] = mValues[i]*old + color[i]*amountNew; 
-		}
-	}
-	
 }
